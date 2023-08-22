871ecf56eb6215f34765f5526388ac908d1ca918
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-22 21:55:37 +0530

    22-aug

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API = axios.create({ baseUrl: "http://localhost:5000" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const login = (authData) => API.post("/user/login", authData);
-export const signUp = (authData) => API.post("/user/signup", authData);
+export const signUp = (authData) => {
+  console.log(authData);
+  API.post("/user/signup", authData);
+};
