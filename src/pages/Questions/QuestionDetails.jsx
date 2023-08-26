b3ee98f8233be10afd2ebac7198baf8f6fd8b512
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-26 12:40:01 +0530

    update-profile

--- src/pages/Questions/QuestionDetails.jsx
+++ src/pages/Questions/QuestionDetails.jsx
@@ -1,84 +1,87 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams, Link, useNavigate, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import moment from "moment";
+import copy from "copy-to-clipboard";
+
 import upvote from "../../assets/sort-up.svg";
 import downvote from "../../assets/sort-down.svg";
 import "./Questions.css";
 import Avatar from "../../components/Avatar/Avatar";
-import DisplayAnswers from "./DisplayAnswers";
+import DisplayAnswer from "./DisplayAnswer";
+import {
+  postAnswer,
+  deleteQuestion,
+  voteQuestion,
+} from "../../actions/question";
 
-const QuestionDetails = () => {
+const QuestionsDetails = () => {
   const { id } = useParams();
+  const questionsList = useSelector((state) => state.questionsReducer);
 
-  var questionsList = [
-    {
-      id: "1",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 2,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["java", "node js", "react js", "mongo db", "express js"],
-      userPosted: "mano",
-      userId: 1,
-      askedOn: "jan 1",
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-    {
-      id: "2",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 0,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["javascript", "R", "python"],
-      userPosted: "mano",
-      askedOn: "jan 1",
-      userId: 1,
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-    {
-      id: "3",
-      upVotes: 3,
-      downVotes: 2,
-      noOfAnswers: 0,
-      questionTitle: "What is a function?",
-      questionBody: "It meant to be",
-      questionTags: ["javascript", "R", "python"],
-      userPosted: "mano",
-      askedOn: "jan 1",
-      userId: 1,
-      answer: [
-        {
-          answerBody: "Answer",
-          userAnswered: "kumar",
-          answeredOn: "jan 2",
-          userId: 2,
-        },
-      ],
-    },
-  ];
+  const [Answer, setAnswer] = useState("");
+  const Navigate = useNavigate();
+  const dispatch = useDispatch();
+  const User = useSelector((state) => state.currentUserReducer);
+  const location = useLocation();
+  const url = "http://localhost:3000";
+
+  const handlePostAns = (e, answerLength) => {
+    e.preventDefault();
+    if (User === null) {
+      alert("Login or Signup to answer a question");
+      Navigate("/Auth");
+    } else {
+      if (Answer === "") {
+        alert("Enter an answer before submitting");
+      } else {
+        dispatch(
+          postAnswer({
+            id,
+            noOfAnswers: answerLength + 1,
+            answerBody: Answer,
+            userAnswered: User.result.name,
+          })
+        );
+        setAnswer("");
+      }
+    }
+  };
+
+  const handleShare = () => {
+    copy(url + location.pathname);
+    alert("Copied url : " + url + location.pathname);
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteQuestion(id, Navigate));
+  };
+
+  const handleUpVote = () => {
+    if (User === null) {
+      alert("Login or Signup to up vote a question");
+      Navigate("/Auth");
+    } else {
+      dispatch(voteQuestion(id, "upVote"));
+    }
+  };
+
+  const handleDownVote = () => {
+    if (User === null) {
+      alert("Login or Signup to down vote a question");
+      Navigate("/Auth");
+    } else {
+      dispatch(voteQuestion(id, "downVote"));
+    }
+  };
 
   return (
     <div className="question-details-page">
-      {questionsList === null ? (
+      {questionsList.data === null ? (
         <h1>Loading...</h1>
       ) : (
         <>
-          {questionsList
+          {questionsList.data
             .filter((question) => question._id === id)
             .map((question) => (
               <div key={question._id}>
@@ -91,15 +94,15 @@ const QuestionDetails = () => {
                         alt=""
                         width="18"
                         className="votes-icon"
+                        onClick={handleUpVote}
                       />
-                      <p>
-                        {question.upVotes.length - question.downVotes.length}
-                      </p>
+                      <p>{question.upVote.length - question.downVote.length}</p>
                       <img
                         src={downvote}
                         alt=""
                         width="18"
                         className="votes-icon"
+                        onClick={handleDownVote}
                       />
                     </div>
                     <div style={{ width: "100%" }}>
@@ -111,12 +114,17 @@ const QuestionDetails = () => {
                       </div>
                       <div className="question-actions-user">
                         <div>
-                          <button type="button">Share</button>
-
-                          <button type="button">Delete</button>
+                          <button type="button" onClick={handleShare}>
+                            Share
+                          </button>
+                          {User?.result?._id === question?.userId && (
+                            <button type="button" onClick={handleDelete}>
+                              Delete
+                            </button>
+                          )}
                         </div>
                         <div>
-                          <p>asked {question.askedOn}</p>
+                          <p>asked {moment(question.askedOn).fromNow()}</p>
                           <Link
                             to={`/Users/${question.userId}`}
                             className="user-link"
@@ -137,35 +145,55 @@ const QuestionDetails = () => {
                     </div>
                   </div>
                 </section>
-
                 {question.noOfAnswers !== 0 && (
                   <section>
-                    <h3>{question.noOfAnswers} answers</h3>
-                    <DisplayAnswers key={question._id} question={question} />
+                    <h3>{question.noOfAnswers} Answers</h3>
+                    <DisplayAnswer
+                      key={question._id}
+                      question={question}
+                      handleShare={handleShare}
+                    />
                   </section>
                 )}
-
                 <section className="post-ans-container">
                   <h3>Your Answer</h3>
-                  <form action="">
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                  <form
+                    onSubmit={(e) => {
+                      handlePostAns(e, question.answer.length);
+                    }}
+                  >
+                    <textarea
+                      name=""
+                      id=""
+                      cols="30"
+                      rows="10"
+                      value={Answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                    ></textarea>
+                    <br />
                     <input
                       type="submit"
                       className="post-ans-btn"
-                      value="post your answer"
+                      value="Post Your Answer"
                     />
                   </form>
-                  <p>Browse other Question tagged</p>
-                  {question.questionTags.map((tag) => (
-                    <Link to="/Tags" key={tag} className="ans-tags">
-                      {tag}
+                  <p>
+                    Browse other Question tagged
+                    {question.questionTags.map((tag) => (
+                      <Link to="/Tags" key={tag} className="ans-tags">
+                        {" "}
+                        {tag}{" "}
+                      </Link>
+                    ))}{" "}
+                    or
+                    <Link
+                      to="/AskQuestion"
+                      style={{ textDecoration: "none", color: "#009dff" }}
+                    >
+                      {" "}
+                      ask your own question.
                     </Link>
-                  ))}{" "}
-                  or{" "}
-                  <Link
-                    to="/AskQuestion"
-                    style={{ textDecoration: "none", color: "#009dff" }}
-                  ></Link>
+                  </p>
                 </section>
               </div>
             ))}
@@ -175,4 +203,4 @@ const QuestionDetails = () => {
   );
 };
 
-export default QuestionDetails;
+export default QuestionsDetails;
